fe9b5b9fe69e78d4d6e424c90e5d0ba8552763b0
h19e <yusukemunakata@gmail.com>
2010-07-15 01:00:05 +0900

    init script add

--- exec/init.sh
+++ exec/init.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 echo "init"
+
+HTDOCS_DIR=$1
+APP_DIR=$2
+
+cp ../src/htdocs/index.php $HTDOCS_DIR/index.php
+
--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "Hello CustomForm";
+echo "Hello CustomForm alpha";
