e0a6a65ce1b6f88c8e2fc5d54d1d439674155c7f
h19e <yusukemunakata@gmail.com>
2010-07-17 16:00:25 +0900

    db

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -1,3 +1,20 @@
 <?php
 
 echo "Hello CustomForm alpha";
+
+
+try {
+	$pdo = new PDO("mysql:host=localhost; dbname=sampledb","sample_user","sample_user");
+
+	$stmt = $pdo->query('select * from db_test ');
+
+	while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+		var_dump($row);
+	}
+} catch (PDOException $e) {
+	var_dump($e->getMessage());
+}
+
+
+
+
