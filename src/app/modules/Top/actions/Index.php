3b028bf651f925de6389a0a0d920804693b9ed20
h19e <yusukemunakata@gmail.com>
2010-07-20 23:16:09 +0900

    module

---
+++ src/app/modules/Top/actions/Index.php
@@ -0,0 +1,21 @@
+<?php
+
+class Action_Index
+{
+	public function execute()
+	{
+
+		try {
+			$pdo = new PDO("mysql:host=" . DB_HOST ."; dbname=sampledb",DB_USER,DB_PASS);
+
+			$stmt = $pdo->query('select * from db_test ');
+
+			while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+				var_dump($row);
+			}
+		} catch (PDOException $e) {
+			var_dump($e->getMessage());
+		}
+
+	}
+}
